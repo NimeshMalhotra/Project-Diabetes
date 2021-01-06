921e41900130f963bb3310f94239a90a7186c9fb
NimeshMalhotra <47629245+NimeshMalhotra@users.noreply.github.com>
2021-01-06 12:07:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def predict():
     prediction=model.predict_proba(row_df)
     output='{0:.{1}f}'.format(prediction[0][1], 2)
     output = str(float(output)*100)+'%'
-    if output>str(5.0):
+    if output>str(0.5):
         return render_template('result.html',pred=f'You have chance of having diabetes.\nProbability of having Diabetes is {output}')
     else:
         return render_template('result.html',pred=f'You are safe.\n Probability of having diabetes is {output}')
